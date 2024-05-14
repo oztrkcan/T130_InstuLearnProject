80a498c6c808398ee7183948cae875d12c46ada8
canozturk <canozturk91@gmail.com>
2024-05-14 20:36:05 +0300

    first pre-settings of the sprint 2

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,21 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/html-reports/rapor.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "",
+        dryRun = true
+)
+public class Runner {
+
+
+}
