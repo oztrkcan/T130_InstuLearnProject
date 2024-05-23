2615445b25cbe5db44625f59058a3c0fcadf53c6
batu <batuhany20@gmail.com>
2024-05-23 22:13:17 +0300

    Batuhan 23.05.2024 V01

--- src/test/java/pages/StorePage.java
+++ src/test/java/pages/StorePage.java
@@ -1,9 +1,148 @@
 package pages;
 
+import com.github.javafaker.Faker;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utilities.ReusableMethods;
 
 public class StorePage extends BasePage{
     public StorePage(WebDriver driver) {
         super(driver);
+    }
+
+    public static StorePage storePage=new StorePage(driver);
+
+    /* Batuhan burada çalıştı */
+    @FindBy(xpath = "//*[text()='Store']")
+    public WebElement storeText;
+
+    @FindBy(xpath = "//*[text()='Products']")
+    public WebElement productsText;
+
+    public void storeDisableAndClick(){
+        Assert.assertTrue(storePage.storeText.isDisplayed());
+        storePage.storeText.click();
+    }
+
+    public void productsTextIsDisplayed(){
+        Assert.assertTrue(productsText.isDisplayed()
+        );
+    }
+
+    @FindBy(xpath = "//*[@placeholder=\"Search in store\"]")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "(//*[@type=\"submit\"])[2]")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "(//*[@class=\"product-card\"])[1]")
+    public WebElement firstProductInSearch;
+
+    @FindBy(xpath = "//*[@class=\"mt-15 product-title font-weight-bold font-16 text-dark-blue\"]")
+    public WebElement firstProduct;
+
+    @FindBy(xpath = "//*[text()='Virtual']")
+    public WebElement firstFilterButton;
+
+    @FindBy(xpath = "//*[text()='Physical']")
+    public WebElement secondFilterButton;
+
+    @FindBy(xpath = "//*[text()='Only available products']")
+    public WebElement thirdFilterButton;
+
+    @FindBy(xpath = "(//*[text()='Filter items'])[1]")
+    public WebElement firstFilterSearchButton;
+
+    @FindBy(xpath = "(//*[text()='Filter items'])[2]")
+    public WebElement secondFilterSearchButton;
+
+    public void searchBoxIsDisabledAndSearch(String metin){
+        Assert.assertTrue(searchBox.isDisplayed());
+        ReusableMethods.sendKeys(searchBox,metin);
+        searchButton.click();
+    }
+
+    public void filterDisabledAndChoose(){
+        firstFilterButton.click();
+        secondFilterButton.click();
+        thirdFilterButton.click();
+        firstFilterSearchButton.click();
+        ReusableMethods.wait(2);
+    }
+
+    public void firstProductChoose(){
+        ReusableMethods.click(firstProduct);
+    }
+
+    @FindBy(xpath = "(//*[@type='submit'])[2]")
+    public WebElement addToCartButton;
+
+    @FindBy(xpath = "(//*[@type='button'])[8]")
+    public WebElement buyNowButton;
+
+    public void disabledAddToCart(){
+        Assert.assertTrue(addToCartButton.isDisplayed());
+
+    }
+
+    @FindBy(xpath = "//*[text()='Checkout']")
+    public WebElement checkoutButton;
+
+    @FindBy(xpath = "//*[@for=\"offline\"]")
+    public WebElement stripeButton;
+
+    @FindBy(xpath = "//*[text()='Start Payment']")
+    public WebElement startPaymentButton;
+
+    @FindBy(xpath = "(//*[@class=\"InputContainer\"])[1]")
+    public WebElement stripeEmailTextBox;
+
+    @FindBy(xpath = "(//*[@class=\"InputContainer\"])[2]")
+    public WebElement stripeCreditCardTextBox;
+
+    @FindBy(xpath = "(//*[@class=\"InputContainer\"])[3]")
+    public WebElement stripeCardDateTextBox;
+
+    @FindBy(xpath = "(//*[@class=\"InputContainer\"])[4]")
+    public WebElement stripeCardCVCTextBox;
+
+    @FindBy(xpath = "(//*[@class=\"InputContainer\"])[5]")
+    public WebElement stripeNameTextBox;
+
+    @FindBy(xpath = "//*[@class=\"SubmitButton-IconContainer\"]")
+    public WebElement stripePayButton;
+
+    @FindBy(xpath = "//*[text()='Congratulations!']")
+    public WebElement congratulationsPaymentText;
+    Faker faker=new Faker();
+
+    public void buyWithStripe(){
+        ReusableMethods.click(stripeButton);
+        // ReusableMethods.waitForClickablility(startPaymentButton);
+        // startPaymentButton.click();
+        // //stripeEmailTextBox.click();
+        // ReusableMethods.sendKeys(stripeEmailTextBox,"batuhan@gmail.com");
+        // ReusableMethods.sendKeys(stripeCreditCardTextBox,"4242424242424242");
+        // ReusableMethods.sendKeys(stripeCardDateTextBox,"1225");
+        // ReusableMethods.sendKeys(stripeCardCVCTextBox,"123");
+        // ReusableMethods.sendKeys(stripeNameTextBox,"batuhan yıldız");
+        ReusableMethods.click(startPaymentButton);
+        ReusableMethods.waitForVisibility(congratulationsPaymentText);
+        Assert.assertTrue(congratulationsPaymentText.isDisplayed());
+
+
+
+    }
+
+    public void disableBuyNowAndClick(){
+        ReusableMethods.click(buyNowButton);
+        ReusableMethods.click(checkoutButton);
+    }
+
+    public void buyProduct(){
+        buyWithStripe();
+
     }
 }
--- src/test/java/stepdefinitions/StorePageSteps.java
+++ src/test/java/stepdefinitions/StorePageSteps.java
@@ -1,4 +1,52 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Then;
+import pages.StorePage;
+
 public class StorePageSteps extends BaseStep{
+
+    StorePage storePage=new StorePage(driver);
+
+    @Then("I see the Store and click.")
+    public void i_see_the_store_and_click() {
+        storePage.storeDisableAndClick();
+
+    }
+
+    @Then("I see the Products text.")
+    public void i_see_the_products_text() {
+        storePage.productsTextIsDisplayed();
+
+    }
+
+    @Then("I see the Search Box and search for {string}.")
+    public void i_see_the_search_box_and_search_for(String string) {
+        storePage.searchBoxIsDisabledAndSearch(string);
+
+    }
+
+    @Then("I see Filters and I filter.")
+    public void i_see_filters_and_i_filter() {
+        storePage.filterDisabledAndChoose();
+    }
+
+    @Then("I click the first product that appear.")
+    public void i_click_the_first_product_that_appear() {
+        storePage.firstProductChoose();
+    }
+
+    @Then("I see Add to Cart.")
+    public void i_see_add_to_cart() {
+        storePage.disabledAddToCart();
+    }
+
+    @Then("I see Buy now and click.")
+    public void i_see_buy_now_and_click() {
+        storePage.disableBuyNowAndClick();
+    }
+
+    @Then("I buy the product.")
+    public void i_buy_the_product() {
+        storePage.buyProduct();
+    }
 }
