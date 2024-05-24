aa0aced4cd25962ab8d0dde212e3c2f52331e0b4
Muammer ONER <muammer10er@gmail.com>
2024-05-24 18:17:52 +0200

    sonn

---
+++ src/test/java/apis/SpecBuilder.java
@@ -0,0 +1,23 @@
+package apis;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import utilities.TestData.TestDataBase;
+
+public class SpecBuilder {
+    public static RequestSpecification getRequestSpec(){
+        return new RequestSpecBuilder().
+                setBaseUri(TestDataBase.baseUrl).
+                log(LogDetail.ALL).
+                build();
+    }
+
+    public static ResponseSpecification getResponseSpec(){
+        return new ResponseSpecBuilder().
+                log(LogDetail.ALL).
+                build();
+    }
+}
---
+++ src/test/java/constants/MyConstants.java
@@ -0,0 +1,18 @@
+package constants;
+
+public class MyConstants {
+    public static final String STORE = "/store";
+    public static final String baseUrl = "https://qa.instulearn.com";
+    public static final String homePageUrl = "https://qa.instulearn.com";
+    public static final String coursesPage =  "classes?sort=newest";
+    public static final String instructorsPage = "/instructors";
+    public static final String storePage = "/products";
+    public static final String blogPage = "/blog";
+    public static final String loginPage="/login";
+    public static final String registerPage=  "/register";
+    public static final String aboutPage= "/pages/about";
+    public static final String contactPage = "/contact";
+    public static final String certificateValidationPage= "/certificate_validation";
+    public static final String termsPage = "/pages/terms";
+    public static final String dashboardPage = "/panel";
+}
